f7b20c44f6a5099f51eed388cf3dcdfda340f0db
Max Warner <m@xwarner.com>
2015-07-25 17:32:47 +0100

    Set Up

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Language</title>
+<link rel="stylesheet" href="css/main.css" type="text/css"/>
+<script src="app/app.js"></script>
+</head>
+<body>
+<h1>Language</h1>
+</body>
+</html>
